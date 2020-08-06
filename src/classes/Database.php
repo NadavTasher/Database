294c26fd71c935d3e85a9973a1d8b840aa94a4d9
nadav <tashernadav@gmail.com>
2020-08-06 22:55:01 +0300

    New database implementation with tokens and access scopes

--- src/classes/Database.php
+++ src/classes/Database.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright (c) 2019 Nadav Tasher
+ * Copyright (c) 2020 Nadav Tasher
  * https://github.com/NadavTasher/Database/
  **/
 
@@ -15,211 +15,140 @@ class Database
     private const ROOT = "/opt";
 
     /**
-     * Handles actions passed with parameters.
+     * Handles database requests with action and parameters.
      * @param string $action Action
-     * @param object $parameters Parameters
+     * @param array $parameters Parameters
      * @return mixed Result
      */
     public static function handle($action, $parameters)
     {
-        // Check scope parameter
-        if (!isset($parameters->scope) || empty($parameters->scope))
-            throw new Error("Invalid scope parameter");
+        // Make sure the request has a token parameter
+        if (!isset($parameters->token) || empty($parameters->token))
+            throw new Error("Missing token parameter");
 
-        // Create the path for the scope
-        $scope = self::child($parameters->scope, self::ROOT);
+        // Make sure the token is rightfully signed
+        $authorized = Token::validate($parameters->token);
 
-        if ($action === "insertScope") {
-            // Make sure the scope does not exist
-            if (file_exists($scope))
-                throw new Error("Scope already exists");
+        // Create a request scope
+        $requested = array();
 
-            // Create the scope
-            self::insert($scope);
+        // Check whether the scope parameter is set
+        if (isset($parameters->scope) && !empty($parameters->scope)) {
+            // Push scope parameter to requested scope
+            array_push($requested, $parameters->scope);
 
-            // Return success
-            return null;
+            // Check whether the table parameter is set
+            if (isset($parameters->table) && !empty($parameters->table)) {
+                // Push table parameter to requested scope
+                array_push($requested, $parameters->table);
+
+                // Check whether the entry parameter is set
+                if (isset($parameters->entry) && !empty($parameters->entry)) {
+                    // Push entry parameter to requested scope
+                    array_push($requested, $parameters->entry);
+                }
+            }
         }
 
-        if ($action === "removeScope") {
-            // Make sure the scope exists
-            if (!file_exists($scope))
+        // Make sure the requested scope is within the control scope of the token
+        if (count($authorized) > count($requested))
+            throw new Error("Unauthorized token scope");
+
+        foreach ($authorized as $index => $value)
+            if ($requested[$index] !== $value)
+                throw new Error("Unauthorized token scope");
+
+        // Assemble a path from the requested scope
+        $path = self::ROOT;
+
+        // Loop over children and append to path
+        foreach ($requested as $scope) {
+            // Make sure the parent path exists before appending the child path
+            if (!file_exists($path))
                 throw new Error("Scope does not exist");
 
-            // Create the scope
-            self::remove($scope);
+            // Append child to path
+            $path .= DIRECTORY_SEPARATOR . bin2hex($scope);
+        }
+
+        // Check whether the requested action is "check"
+        if ($action === "check") {
+            // Check whether the path exists
+            return file_exists($path);
+        }
+
+        // Check whether the requested action is "insert"
+        if ($action === "insert") {
+            // Make sure the path does not exist
+            if (file_exists($path))
+                throw new Error("Scope already exists");
+
+            // Create the path
+            self::touch($path);
+
+            // Issue and return a token
+            return Token::issue($requested);
+        }
+
+        // Check whether the requested action is "remove"
+        if ($action === "remove") {
+            // Make sure the path exists
+            if (!file_exists($path))
+                throw new Error("Scope does not exist");
+
+            // Create the path
+            self::unlink($path);
 
             // Return success
-            return null;
+            return true;
         }
 
-        if ($action === "checkScope") {
-            // Check if scope exists
-            return file_exists($scope);
-        }
-
-        // Make sure the scope exists
-        if (!file_exists($scope))
+        // Make sure the requested scope exists before allowing keystore access
+        if (!file_exists($path))
             throw new Error("Scope does not exist");
 
-        // Check table parameter
-        if (!isset($parameters->table) || empty($parameters->table))
-            throw new Error("Invalid table parameter");
+        // Make sure the requested scope points to an entry before allowing keystore access
+        if (count($requested) === 3) {
+            // Make sure the request has a key parameter
+            if (!isset($parameters->key) || empty($parameters->key))
+                throw new Error("Missing key parameter");
 
-        // Create the path for the table
-        $table = self::child($parameters->table, $scope);
-
-        if ($action === "insertTable") {
-            // Make sure the table does not exist
-            if (file_exists($table))
-                throw new Error("Table already exists");
-
-            // Create the table
-            self::insert($table);
-
-            // Return success
-            return null;
-        }
-
-        if ($action === "removeTable") {
-            // Make sure the table exists
-            if (!file_exists($table))
-                throw new Error("Table does not exist");
-
-            // Create the table
-            self::remove($table);
-
-            // Return success
-            return null;
-        }
-
-        if ($action === "checkTable") {
-            // Check if table exists
-            return file_exists($table);
-        }
-
-        // Make sure the table exists
-        if (!file_exists($table))
-            throw new Error("Table does not exist");
-
-        // Check entry parameter
-        if (!isset($parameters->entry) || empty($parameters->entry))
-            throw new Error("Invalid entry parameter");
-
-        // Create the path for the entry
-        $entry = self::child($parameters->entry, $table);
-
-        if ($action === "insertEntry") {
-            // Make sure the entry does not exist
-            if (file_exists($entry))
-                throw new Error("Entry already exists");
-
-            // Create the entry
-            self::insert($entry);
-
-            // Return success
-            return null;
-        }
-
-        if ($action === "removeEntry") {
-            // Make sure the entry exists
-            if (!file_exists($entry))
-                throw new Error("Entry does not exist");
-
-            // Create the entry
-            self::remove($entry);
-
-            // Return success
-            return null;
-        }
-
-        if ($action === "checkEntry") {
-            // Check if entry exists
-            return file_exists($entry);
-        }
-
-        // Make sure the entry exists
-        if (!file_exists($entry))
-            throw new Error("Entry does not exist");
-
-        // Check cell parameter
-        if (!isset($parameters->cell) || empty($parameters->cell))
-            throw new Error("Invalid cell parameter");
-
-        // Create the path for the cell
-        $cell = self::child($parameters->cell, $entry);
-
-        if ($action === "insertCell") {
-            // Make sure the cell does not exist
-            if (file_exists($cell))
-                throw new Error("Cell already exists");
-
-            // Create the cell
-            touch($cell);
-
-            // Return success
-            return null;
-        }
-
-        if ($action === "removeCell") {
-            // Make sure the cell exists
-            if (!file_exists($cell))
-                throw new Error("Cell does not exist");
-
-            // Create the cell
-            unlink($cell);
-
-            // Return success
-            return null;
-        }
-
-        if ($action === "checkCell") {
-            // Check if cell exists
-            return file_exists($cell);
-        }
-
-        // Make sure the cell exists
-        if (!file_exists($cell))
-            throw new Error("Cell does not exist");
-
-        if ($action === "readCell") {
-            // Read cell and return
-            return file_get_contents($cell);
-        }
-
-        if ($action === "writeCell") {
-            // Check value parameter
-            if (!isset($parameters->value))
+            // Make sure the request has a value parameter
+            if (!isset($parameters->value) || empty($parameters->value))
                 throw new Error("Missing value parameter");
 
-            // Write cell
-            file_put_contents($cell, $parameters->value);
+            // Extend the path
+            $path .= DIRECTORY_SEPARATOR . bin2hex($parameters->key);
 
-            // Return success
-            return null;
+            // Check whether the requested action is "read"
+            if ($action === "read") {
+                // Check whether the path exists
+                if (!file_exists($path))
+                    // Return fallback value
+                    return $parameters->value;
+
+                // Read path and return
+                return file_get_contents($path);
+            }
+
+            // Check whether the requested action is "write"
+            if ($action === "write") {
+                // Write path
+                file_put_contents($path, $parameters->value);
+
+                // Return success
+                return true;
+            }
         }
 
-        // Throw unknown error
         throw new Error("Unknown action");
     }
 
     /**
-     * Returns a child path for a given base path.
-     * @param string $child Child path
-     * @param string $parent Parent path
-     * @return string Path
-     */
-    public static function child($child, $parent)
-    {
-        // Return combined path
-        return $parent . DIRECTORY_SEPARATOR . bin2hex($child);
-    }
-
-    /**
-     * Creates directories.
+     * Makes directories.
      * @param string $path Path
      */
-    public static function insert($path)
+    private static function touch($path)
     {
         // Create directory
         mkdir($path);
@@ -229,20 +158,25 @@ class Database
      * Removes directories.
      * @param string $path Path
      */
-    public static function remove($path)
+    private static function unlink($path)
     {
         // Check whether the path is a directory
         if (is_file($path)) {
             // Unlink file
             unlink($path);
         } else {
-            // List subdirectories
-            foreach (array_slice(scandir($path), 2) as $value) {
-                self::remove($value);
-            }
+            // List child paths
+            $paths = scandir($path);
+
+            // Slice child paths
+            $paths = array_slice($paths, 2);
+
+            // Unlink child paths
+            foreach ($paths as $value)
+                self::unlink($value);
+
             // Remove directory
             rmdir($path);
         }
     }
-
 }
--- src/index.php
+++ src/index.php
@@ -6,16 +6,10 @@
  **/
 
 include_once __DIR__ . DIRECTORY_SEPARATOR . "classes" . DIRECTORY_SEPARATOR . "Base.php";
+include_once __DIR__ . DIRECTORY_SEPARATOR . "classes" . DIRECTORY_SEPARATOR . "Token.php";
 include_once __DIR__ . DIRECTORY_SEPARATOR . "classes" . DIRECTORY_SEPARATOR . "Database.php";
 
-const PASSWORD_VARIABLE = "DATABASE_PASSWORD";
-
 Base::handle(function ($action, $parameters) {
-    // Check password environment variable
-    if (getenv(PASSWORD_VARIABLE))
-        if (!isset($parameters->password) || getenv(PASSWORD_VARIABLE) !== $parameters->password)
-            throw new Error("Invalid password parameter");
-
     // Handle database requests
     return Database::handle($action, $parameters);
 });
